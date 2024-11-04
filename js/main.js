9c76533ad9e0ffde21ab36e2357cec33fd9cddec
Andreas-Mollberg <andreas.mollberg@gmail.com>
2024-11-04 18:20:14 +0100

    Code update
    reworked hamburger menu

--- js/main.js
+++ js/main.js
@@ -48,6 +48,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const navMenu = document.getElementById('nav-menu');
 
     hamburger.addEventListener('click', function() {
+        hamburger.classList.toggle('active');
         navMenu.classList.toggle('active');
     });
 });
